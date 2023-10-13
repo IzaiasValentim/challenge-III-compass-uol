e0ce79e3352d8067d21dbc05146665b81ea9d265
IzaiasValentim <izaias451@gmail.com>
2023-10-13 20:32:55 -0300

    feat/added the base project

---
+++ ms-improvements/src/main/java/br/izaias/valentim/msimprovements/MsImprovementsApplication.java
@@ -0,0 +1,13 @@
+package br.izaias.valentim.msimprovements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsImprovementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsImprovementsApplication.class, args);
+	}
+
+}
---
+++ ms-improvements/src/test/java/br/izaias/valentim/msimprovements/MsImprovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.izaias.valentim.msimprovements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsImprovementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
