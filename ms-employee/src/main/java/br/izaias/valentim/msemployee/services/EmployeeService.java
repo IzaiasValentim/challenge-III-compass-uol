ffe0c635919f5c5e4a03a569204b6d85c5d3f071
IzaiasValentim <izaias451@gmail.com>
2023-10-13 20:29:17 -0300

    feat/create the Service for Employee.

---
+++ ms-employee/src/main/java/br/izaias/valentim/msemployee/services/EmployeeService.java
@@ -0,0 +1,85 @@
+package br.izaias.valentim.msemployee.services;
+
+import br.izaias.valentim.msemployee.entities.Employee;
+import br.izaias.valentim.msemployee.repositories.EmployeeJpaRepository;
+import br.izaias.valentim.msemployee.utils.CpfValidator;
+import jakarta.persistence.PersistenceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.xml.crypto.Data;
+import java.net.URI;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeJpaRepository repository;
+    private final CpfValidator cpfValidator;
+
+    @Autowired
+    public EmployeeService(EmployeeJpaRepository repository, CpfValidator cpfValidator) {
+        this.repository = repository;
+        this.cpfValidator = cpfValidator;
+    }
+
+    public ResponseEntity create(Employee employeeToSave) {
+        try {
+            cpfValidator.validateCpf(employeeToSave.getCpf());
+            repository.save(employeeToSave);
+            URI readerLocation = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .query("cpf={cpf}")
+                    .buildAndExpand(employeeToSave.getCpf())
+                    .toUri();
+            return ResponseEntity.created(readerLocation).build();
+
+        } catch (ResponseStatusException rEx) {
+            return ResponseEntity.status(rEx.getStatusCode()).body(rEx.getMessage());
+        }catch (DataAccessException exData){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("cpf already registered");
+        }
+    }
+
+    public List<Employee> gelAllEmployees() {
+        return repository.findAll();
+    }
+
+    public Employee getByCpf(String cpf) {
+        if (cpfValidator.validateCpf(cpf))
+            return repository.getEmployeeByCpf(cpf);
+        return null;
+    }
+
+    public ResponseEntity updateEmployee(String cpf, String newName, String newUserRole) {
+
+        if (cpfValidator.validateCpf(cpf)) {
+            Employee employeeOnDB = repository.getEmployeeByCpf(cpf);
+
+            if (employeeOnDB == null) {
+                return null;
+            }
+            employeeOnDB.setName(newName);
+            employeeOnDB.setUserRole(newUserRole);
+            repository.save(employeeOnDB);
+            ResponseEntity.noContent();
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("INVALID CPF");
+    }
+
+    public Boolean deleteEmployee(String cpf) {
+        if (cpfValidator.validateCpf(cpf)) {
+            Employee consultEmployee = repository.getEmployeeByCpf(cpf);
+            if (consultEmployee != null) {
+                repository.delete(consultEmployee);
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
