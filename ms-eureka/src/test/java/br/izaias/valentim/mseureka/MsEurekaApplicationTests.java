d04fb4634ad2161d3f8b2eaac9732fb33123df8b
IzaiasValentim <izaias451@gmail.com>
2023-10-14 14:45:45 -0300

    feat/added the eureka and gateway projects

---
+++ ms-eureka/src/test/java/br/izaias/valentim/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.izaias.valentim.mseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
