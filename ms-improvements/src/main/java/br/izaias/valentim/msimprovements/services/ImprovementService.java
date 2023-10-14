2c1c108744783bdd03f21710835eb48a817c19de
IzaiasValentim <izaias451@gmail.com>
2023-10-14 14:40:43 -0300

    refactor/ added a function async in the flow of create a new improvement.

--- ms-improvements/src/main/java/br/izaias/valentim/msimprovements/services/ImprovementService.java
+++ ms-improvements/src/main/java/br/izaias/valentim/msimprovements/services/ImprovementService.java
@@ -4,12 +4,13 @@ import br.izaias.valentim.msimprovements.entities.Improvement;
 import br.izaias.valentim.msimprovements.entities.Voute;
 import br.izaias.valentim.msimprovements.repositories.ImprovementRepository;
 import br.izaias.valentim.msimprovements.services.exceptions.PersistenceException;
+import br.izaias.valentim.msimprovements.utils.SchedulingImprovementsVoute;
 import jakarta.transaction.Transactional;
-import org.aspectj.weaver.ast.Var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,24 +18,43 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@EnableAsync
 public class ImprovementService {
     private final ImprovementRepository repository;
     private final VouteService vouteService;
+    private final SchedulingImprovementsVoute timer;
 
     @Autowired
-    public ImprovementService(ImprovementRepository repository, VouteService vouteService) {
+    public ImprovementService(ImprovementRepository repository, VouteService vouteService, SchedulingImprovementsVoute timer) {
         this.repository = repository;
         this.vouteService = vouteService;
+        this.timer = timer;
     }
 
     @Transactional
     public Improvement createImprovement(Improvement improvementToCreate) {
         try {
             improvementToCreate.setResult(Improvement.Result.IN_PROGRESS);
-            return repository.save(improvementToCreate);
 
+            Improvement criado = repository.save(improvementToCreate);
+
+            timer.execute(criado);
+
+            return criado;
         } catch (DataAccessException exData) {
             throw new PersistenceException("Error at create improvement", exData);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    public void closeImprovementSessionOfVote(Improvement improvementToCloseSession) {
+        try {
+            improvementToCloseSession.setResult(Improvement.Result.CLOSED);
+            repository.save(improvementToCloseSession);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "SESSÃO FECHADA");
         }
     }
 
@@ -82,7 +102,7 @@ public class ImprovementService {
             vouteCreate = new Voute(Voute.vouteValue.Rejected, cpf);
 
         Optional<Improvement> getImprovement = repository.findById(idImprovement);
-        if (getImprovement.isPresent() && getImprovement.get().vouteIsUnique(cpf)) {
+        if (getImprovement.isPresent() && getImprovement.get().vouteIsUnique(cpf) && (getImprovement.get().getResult().equals(Improvement.Result.IN_PROGRESS))) {
 
             getImprovement.get().getVoutes().add(vouteCreate);
             return ResponseEntity.ok().build();
