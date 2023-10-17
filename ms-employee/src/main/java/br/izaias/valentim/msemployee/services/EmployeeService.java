dbd5d57ecf79ce58fd46d4f5680e76b2ebc5dede
IzaiasValentim <izaias451@gmail.com>
2023-10-17 13:41:34 -0300

    refactor/ implements treatment os exception to Service, the logic stay the same.

--- ms-employee/src/main/java/br/izaias/valentim/msemployee/services/EmployeeService.java
+++ ms-employee/src/main/java/br/izaias/valentim/msemployee/services/EmployeeService.java
@@ -2,18 +2,20 @@ package br.izaias.valentim.msemployee.services;
 
 import br.izaias.valentim.msemployee.entities.Employee;
 import br.izaias.valentim.msemployee.repositories.EmployeeJpaRepository;
+import br.izaias.valentim.msemployee.services.exceptions.EmployeeNotFoundException;
+import br.izaias.valentim.msemployee.services.exceptions.InvalidCpfException;
+import br.izaias.valentim.msemployee.services.exceptions.PersistenceException;
 import br.izaias.valentim.msemployee.utils.CpfValidator;
-import jakarta.persistence.PersistenceException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.xml.crypto.Data;
 import java.net.URI;
 import java.util.List;
 
@@ -27,10 +29,15 @@ public class EmployeeService {
         this.repository = repository;
         this.cpfValidator = cpfValidator;
     }
+
     @Transactional
     public ResponseEntity create(Employee employeeToSave) {
         try {
             cpfValidator.validateCpf(employeeToSave.getCpf());
+
+            if (repository.getEmployeeByCpf(employeeToSave.getCpf()) != null) {
+                throw new PersistenceException("CPF ALREADY REGISTERED IN THE SYSTEM");
+            }
             repository.save(employeeToSave);
             URI readerLocation = ServletUriComponentsBuilder
                     .fromCurrentRequest()
@@ -39,10 +46,10 @@ public class EmployeeService {
                     .toUri();
             return ResponseEntity.created(readerLocation).build();
 
-        } catch (ResponseStatusException rEx) {
-            return ResponseEntity.status(rEx.getStatusCode()).body(rEx.getMessage());
-        }catch (DataAccessException exData){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("cpf already registered");
+        } catch (InvalidCpfException invalidCpf) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(invalidCpf.getMessage());
+        } catch (DataAccessException exData) {
+            throw new PersistenceException("ERROR WHEN CREATING EMPLOYEE: " + exData.getMessage());
         }
     }
 
@@ -50,37 +57,70 @@ public class EmployeeService {
         return repository.findAll();
     }
 
-    public Employee getByCpf(String cpf) {
-        if (cpfValidator.validateCpf(cpf))
-            return repository.getEmployeeByCpf(cpf);
-        return null;
+    public ResponseEntity getByCpf(String cpf) {
+        try {
+            cpfValidator.validateCpf(cpf);
+
+            Employee employeeFinds = repository.getEmployeeByCpf(cpf);
+
+            if (employeeFinds == null)
+                throw new EmployeeNotFoundException("EMPLOYEE NOT FOUND");
+
+            return ResponseEntity.ok(employeeFinds);
+
+        } catch (InvalidCpfException invalidCpf) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, invalidCpf.getMessage());
+        }
     }
+
     @Transactional
     public ResponseEntity updateEmployee(String cpf, String newName, String newUserRole) {
 
-        if (cpfValidator.validateCpf(cpf)) {
-            Employee employeeOnDB = repository.getEmployeeByCpf(cpf);
+        try {
+            cpfValidator.validateCpf(cpf);
 
-            if (employeeOnDB == null) {
-                return ResponseEntity.notFound().build();
-            }
-            employeeOnDB.setName(newName);
-            employeeOnDB.setUserRole(newUserRole);
+            Employee employeeOnDB = repository.getEmployeeByCpf(cpf);
+            if (employeeOnDB == null)
+                throw new EmployeeNotFoundException("EMPLOYEE NOT FOUND");
+
+            if (!newName.isEmpty())
+                employeeOnDB.setName(newName);
+
+            if (!newUserRole.isEmpty())
+                employeeOnDB.setUserRole(newUserRole);
+
             repository.save(employeeOnDB);
+
             return ResponseEntity.noContent().build();
+
+        } catch (InvalidCpfException invalidCpf) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, invalidCpf.getMessage());
+
+        } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    dataIntegrityViolationException.getMessage());
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("INVALID CPF");
+
     }
 
-    public Boolean deleteEmployee(String cpf) {
-        if (cpfValidator.validateCpf(cpf)) {
+    public ResponseEntity deleteEmployee(String cpf) {
+        try {
+            cpfValidator.validateCpf(cpf);
+
             Employee consultEmployee = repository.getEmployeeByCpf(cpf);
+
             if (consultEmployee != null) {
                 repository.delete(consultEmployee);
-                return true;
+                return ResponseEntity.ok().build();
             }
+
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "EMPLOYEE NOT FOUND");
+
+        } catch (InvalidCpfException invalidCpf) {
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, invalidCpf.getMessage());
         }
-        return false;
     }
 
 }
